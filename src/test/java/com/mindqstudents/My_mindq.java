3038d2999ad73cc0b563b3b542bf87b5d3f9ff91
Pavankrishna98 <pa1narayanam@gmail.com>
2023-12-07 12:23:15 +0530

    my second push

---
+++ src/test/java/com/mindqstudents/My_mindq.java
@@ -0,0 +1,9 @@
+package com.mindqstudents;
+
+public class My_mindq {
+	public static void main(String[] args) {
+		System.out.println("hi this is mind q systems sr nager");
+		System.out.println("it provide testing training courses in manual and automation");
+	}
+
+}
