f8deb6b3d19520b398637ac4cdc5573ffd87a72b
@93 <suryalakshmi93>
2023-12-07 13:52:19 +0530

    i added in one class

---
+++ src/test/java/com/mindqstudents/Akshara.java
@@ -0,0 +1,11 @@
+package com.mindqstudents;
+
+public class Akshara {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi Akshara");
+
+	}
+
+}
