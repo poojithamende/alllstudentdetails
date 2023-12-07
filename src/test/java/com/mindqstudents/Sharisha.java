b3c40a338c5f463470754dbd36c94d6098a3904f
Sharisha123 <sharisha9@gmail.com>
2023-12-07 12:20:07 +0530

    i added in one class

---
+++ src/test/java/com/mindqstudents/Sharisha.java
@@ -0,0 +1,11 @@
+package com.mindqstudents;
+
+public class Sharisha {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" i am sharisha from hyderabad");
+
+	}
+
+}
---
+++ src/test/java/com/mindqstudents/Thriandh.java
@@ -0,0 +1,8 @@
+package com.mindqstudents;
+
+public class Thriandh {
+	public static void main(String[] args) {
+		System.out.println("hi hello");
+	}
+
+}
