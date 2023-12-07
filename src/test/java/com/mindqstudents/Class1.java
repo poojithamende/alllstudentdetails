2a9c4dd6afa795defebc42902f862fbc59ae6ffe
poojithamende <153080784+poojithamende@users.noreply.github.com>
2023-12-07 11:26:33 +0530

    Add files via upload
    commit in frst

---
+++ src/test/java/com/mindqstudents/Class1.java
@@ -0,0 +1,11 @@
+package com.mindqstudents;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		System.out.println(" this is thrinadh i am  coordinator of mind q systems");
+		System.out.println(" i want know your deatals plz send into my clod repository as soon as possible");
+
+	}
+
+}
