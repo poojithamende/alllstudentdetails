837d4a562cebd9946019f39078aee89000f832ab
Mendepoojitha123 <mendepoojitha@gmail.com>
2023-12-07 13:53:43 +0530

    i am in a new branch

---
+++ src/test/java/com/mindqstudents/Poojithanewgit.java
@@ -0,0 +1,9 @@
+package com.mindqstudents;
+
+public class Poojithanewgit {
+
+	public static void main(String[] args) {
+		System.out.println("i am sucessfully completed the git project");
+	}
+
+}
